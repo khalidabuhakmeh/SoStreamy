9e2ddd83318e481f935594ddb7abd864690b321b
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2013-05-10 14:28:20 -0400

    try to run esentutl from start up.

--- SoStreamy/App_Start/RavenDbConfig.cs
+++ SoStreamy/App_Start/RavenDbConfig.cs
@@ -1,4 +1,8 @@
-﻿using Raven.Client;
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Web.Hosting;
+using Microsoft.Isam.Esent.Interop;
+using Raven.Client;
 using Raven.Client.Embedded;
 using Raven.Client.Indexes;
 
@@ -10,14 +14,36 @@ namespace SoStreamy.App_Start
 
         static RavenDbConfig()
         {
+            try
+            {
+                TryToInitialize();
+            }
+            catch (EsentSecondaryIndexCorruptedException e)
+            {
+                var process = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = "esentutl",
+                        WorkingDirectory = HostingEnvironment.MapPath(ConfigurationManager.AppSettings["RavenDirectory"]),
+                        Arguments = "/d Data",
+                    }
+                };
+                process.Start();
+                TryToInitialize();
+            }
+        }
+
+        private static void TryToInitialize()
+        {
             DocumentStore = new EmbeddableDocumentStore
             {
                 ConnectionStringName = "RavenDb",
                 //UseEmbeddedHttpServer = true,
-                Conventions = { IdentityPartsSeparator = "-" }
+                Conventions = {IdentityPartsSeparator = "-"}
             }.Initialize();
 
-            IndexCreation.CreateIndexes(typeof(RavenDbConfig).Assembly, DocumentStore);
+            IndexCreation.CreateIndexes(typeof (RavenDbConfig).Assembly, DocumentStore);
         }
 
         public static IDocumentStore Start()
